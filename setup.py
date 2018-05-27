d31ec80269208cfd42a0639aaf8b0c10353febfb
JakubBlaha <jakub.blaha@gmail.com>
2018-05-27 19:09:45 +0200

    1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="KivyOnTop",
-    version="1.1",
+    version="1.2",
     author="Jakub Bláha",
     author_email="jakub.blaha@example.com",
     description="Makes Kivy windows stay on top.",
