69c328507dc690c820eef7bd3a1c966d6c0fc9eb
JakubBlaha <jakub.blaha@gmail.com>
2020-05-09 15:43:41 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="KivyOnTop",
-    version="1.3",
+    version="1.4",
     author="Jakub Bláha",
     author_email="jakub.blaha@example.com",
-    description="Makes Kivy windows stay on top.",
+    description="Makes Kivy windows stay on top of other windows.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JakubBlaha/KivyOnTop",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
+        "Operating System :: windows",
     ),
 )
